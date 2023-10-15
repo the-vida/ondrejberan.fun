84e4932a62f5ede23be411a625f31a3e7f9bdf9d
VIDA <czvidayt@gmail.com>
2023-10-15 22:19:07 +0200

    cvv

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-    cookieConsent.addEventListener('click', () => {
+cookieConsent.addEventListener('click', () => {
     player.seekTo(0);  player = new YT.Player('video-frame');
     player.unMute();    
 });
